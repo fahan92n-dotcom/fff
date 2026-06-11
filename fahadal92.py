f0717f05e7aa538841a081128a6ca2d43c8f5039
fahan92n-dotcom <fahan92n@gmail.com>
2026-06-12 01:23:36 +0300

    Update fahadal92.py

--- fahadal92.py
+++ fahadal92.py
@@ -1064,17 +1064,25 @@ else:
 step_survivors[step_num] = passed
 candidates = passed
 
-    global last_complete_short_survivors
-    with last_complete_lock, cascade_stats_lock, cascade_results_lock:
-        for i in range(1, 9):
-            last_complete_stats[i] = dict(cascade_stats.get(i, {}))
-            last_complete_results[i] = dict(cascade_results.get(i, {}))
-        last_complete_survivors = dict(step_survivors)
+global last_complete_short_survivors
 
-    log.info("🎉 إشارات نهائية (LONG): %d", len(candidates))
-    for c in candidates:
-        _fire_signal(c["sym"], c["base_frame"], c["confirm_frame"],
-                    c["triple_frame"], c["df_base"], signal_type="buy")
+with last_complete_lock, cascade_stats_lock, cascade_results_lock:
+    for i in range(1, 9):
+        last_complete_stats[i] = dict(cascade_stats.get(i, {}))
+        last_complete_results[i] = dict(cascade_results.get(i, {}))
+    last_complete_survivors = dict(step_survivors)
+
+log.info("🎉 إشارات نهائية (LONG): %d", len(candidates))
+for c in candidates:
+    _fire_signal(
+        c["sym"],
+        c["base_frame"],
+        c["confirm_frame"],
+        c["triple_frame"],
+        c["df_base"],
+        signal_type="buy"
+    )
+
 
 def run_short_cascade_scan():
     with symbols_cache_lock:
